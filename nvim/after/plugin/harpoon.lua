29bc2e78ee0df61ab706be7eac2ec2fc6be9633d
vzsoares <vinicius.zenha@gmail.com>
2024-10-02 19:00:28 -0300

    config harpoon

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,11 +1,16 @@
 local harpoon = require("harpoon")
 
--- REQUIRED
-harpoon:setup()
--- REQUIRED
+harpoon:setup({
+    settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = true,
+    }
+})
+
+-- keymap
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
