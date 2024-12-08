ea4aa41d9e1ea92079882ae3b5f28f83a3189abf
vzsoares <vinicius.zenha@gmail.com>
2024-12-08 17:19:23 -0300

    feat: use i3status to show getCpuUsage

--- tmux/getCpuUsage.sh
+++ tmux/getCpuUsage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-top -bn1 | grep "Cpu(s)" | \
-           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
-           awk '{printf "%02d%\n", 100 - $1}'
+# top -bn1 | grep "Cpu(s)" | \
+#            sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
+#            awk '{printf "%02d%\n", 100 - $1}'
+
+i3status --run-once -c "~/.config/tmux/.i3status.conf" 
