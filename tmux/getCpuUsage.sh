dc6851b68407d85ec550019f64f021e3bcf98541
vzsoares <vinicius.zenha@gmail.com>
2024-12-10 19:46:29 -0300

    fix: cpu usage

--- tmux/getCpuUsage.sh
+++ tmux/getCpuUsage.sh
@@ -4,4 +4,4 @@
 #            sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
 #            awk '{printf "%02d%\n", 100 - $1}'
 
-i3status --run-once -c "~/.config/tmux/.i3status.conf" 
+i3status -c "$HOME/.config/tmux/.i3status.conf" 
