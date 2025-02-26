7e1428ff1a5225a99ef737af4737a9353410dd0e
vzsoares <vinicius.zenha@gmail.com>
2025-02-25 22:36:03 -0300

    avante key maps

--- nvim/after/plugin/ai.lua
+++ nvim/after/plugin/ai.lua
@@ -21,6 +21,7 @@ require('render-markdown').setup({
 require('avante_lib').load()
 require('avante').setup({
     -- Your config here!
+    -- TODO providers
     provider = "copilot",
     behaviour = {
         enable_cursor_planning_mode = true, -- enable cursor planning mode!
@@ -29,6 +30,38 @@ require('avante').setup({
         width = 40,               -- default % based on available width
         ask = {
             start_insert = false, -- Start insert mode when opening the ask window
+        },
+    },
+    mappings = {
+        --- @class AvanteConflictMappings
+        diff = {
+            ours = "to",
+            theirs = "tt",
+            all_theirs = "ta",
+            both = "tb",
+            cursor = "tt",
+            next = "]x",
+            prev = "[x",
+        },
+        suggestion = {
+            accept = "<M-l>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+        },
+        jump = {
+            next = "]]",
+            prev = "[[",
+        },
+        submit = {
+            normal = "<CR>",
+            insert = "<C-s>",
+        },
+        sidebar = {
+            apply_all = "A",
+            apply_cursor = "a",
+            switch_windows = "<Tab>",
+            reverse_switch_windows = "<S-Tab>",
         },
     },
 })
