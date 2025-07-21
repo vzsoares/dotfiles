9979fc5d2039a192573730f76a4f16039b09fa73
vzsoares <vinicius.zenha@gmail.com>
2025-07-21 20:46:46 -0300

    fix: dlt obsolete

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -35,24 +35,6 @@ return {
         end
     },
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        dependencies = {
-            'neovim/nvim-lspconfig',
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
-            'hrsh7th/nvim-cmp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'saadparwaiz1/cmp_luasnip',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-nvim-lua',
-            'L3MON4D3/LuaSnip',
-            'rafamadriz/friendly-snippets',
-        },
-        lazy = false
-    },
-    {
         'neovim/nvim-lspconfig',
         -- event = { "BufReadPre", "BufNewFile" },
         lazy = false,
