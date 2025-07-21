aa32a325c808547ff1f6def9088acdd6cfcb2662
vzsoares <vinicius.zenha@gmail.com>
2025-07-21 20:39:41 -0300

    feat: file operations

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -274,5 +274,15 @@ return {
                 end,
             })
         end
-    }
+    },
+    {
+        "antosha417/nvim-lsp-file-operations",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-tree.lua",
+        },
+        config = function()
+            require("lsp-file-operations").setup()
+        end,
+    },
 }
