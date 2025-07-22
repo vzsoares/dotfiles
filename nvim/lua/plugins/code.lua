20db3335ccbe9d7f10674e66b11fcd94480dd7e3
vzsoares <vinicius.zenha@gmail.com>
2025-07-21 22:06:28 -0300

    feat: cmp_luasnip

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -96,6 +96,7 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-nvim-lua",
 			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 			"b0o/schemastore.nvim",
 		},
