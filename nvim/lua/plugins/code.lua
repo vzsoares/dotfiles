0494b1bb59942be36754a1fce5973b2b5f7b6659
vzsoares <vinicius.zenha@gmail.com>
2026-03-10 19:58:31 -0300

    chore: rm trigger

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -273,7 +273,7 @@ return {
 
 			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 				callback = function()
-					require("lint").try_lint()
+					-- require("lint").try_lint()
 				end,
 			})
 		end,
