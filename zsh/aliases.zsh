f99789cb2ddf2265a4a95f0120be5b490bc00ec3
vzsoares <vinicius.zenha@gmail.com>
2026-05-21 12:45:59 -0300

    feat(aliases): add combined AWS auth aliases for dev and prod

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,6 +6,9 @@ alias aws-myself="export AWS_PROFILE=myself"
 alias aws-approva-dev-auth="aws login -- profile approva-dev"
 alias aws-approva-prod-auth="aws login -- profile approva-prod"
 
+alias aws-approva-dev-go="export AWS_PROFILE=approva-dev && aws login -- profile approva-dev"
+alias aws-approva-prod-go="export AWS_PROFILE=approva-prod && aws login -- profile approva-prod"
+
 # Tools
 alias ts="tsx"
 alias n="nvim"
