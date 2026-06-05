cbe454228e38238a1151f5439815f830c93e1831
vzsoares <vinicius.zenha@gmail.com>
2026-06-05 19:06:31 -0300

    fix: terraform aws cred

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,11 +3,8 @@ alias aws-approva-dev="export AWS_PROFILE=approva-dev"
 alias aws-approva-prod="export AWS_PROFILE=approva-prod"
 alias aws-myself="export AWS_PROFILE=myself"
 
-alias aws-approva-dev-auth="aws login --profile approva-dev"
-alias aws-approva-prod-auth="aws login --profile approva-prod"
-
-alias aws-approva-dev-go="export AWS_PROFILE=approva-dev && aws login --profile approva-dev"
-alias aws-approva-prod-go="export AWS_PROFILE=approva-prod && aws login --profile approva-prod"
+alias aws-approva-dev-go="export AWS_PROFILE=approva-dev && aws login --profile approva-dev && eval \"\$(aws configure export-credentials --profile approva-dev --format env)\""
+alias aws-approva-prod-go="export AWS_PROFILE=approva-prod && aws login --profile approva-prod && eval \"\$(aws configure export-credentials --profile approva-prod --format env)\""
 
 # Tools
 alias ts="tsx"
